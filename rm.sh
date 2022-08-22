8d27429b20f69681eb6c562c9a7bc35eeac3b994
na.c <nirmal.a.c@greenwayhealth.com>
2022-08-22 16:55:50 +0530

    sdf

--- rm.sh
+++ rm.sh
@@ -5,7 +5,7 @@ RUNNING=$(docker inspect --format="{{ .State.Running }}" $CONTAINER 2> /dev/null
 if [ $? -eq 1 ]; then
   echo "'$CONTAINER' does not exist."
 else
-  /usr/bin/docker rm --force $CONTAINER
+  docker rm --force $CONTAINER
 fi
 
 CONTAINER=mysq
@@ -15,5 +15,5 @@ RUNNING=$(docker inspect --format="{{ .State.Running }}" $CONTAINER 2> /dev/null
 if [ $? -eq 1 ]; then
   echo "'$CONTAINER' does not exist."
 else
-  /usr/bin/docker rm --force $CONTAINER
+  docker rm --force $CONTAINER
 fi
