b967b8404890d443e2c9a1cd09bd5a9d39de6822
na.c <nirmal.a.c@greenwayhealth.com>
2022-08-22 15:14:10 +0530

    gfvhb

--- mm.sh
+++ mm.sh
@@ -1,9 +1,7 @@
 docker build -t nod .
 cd Routes
 docker build -t mysq .
-docker stop no
-docker stop my
-docker rm no
-docker rm my
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
 # docker run --name my -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password -d mysq
 # docker run --name no -p 4000:4000 -d --link my nod
